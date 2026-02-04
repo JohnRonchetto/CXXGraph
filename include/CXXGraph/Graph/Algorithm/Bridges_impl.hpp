7d265cdd3f9cff5ae815e4d4a4084218e214e2c3
John Ronchetto <john4.col@gmail.com>
2026-02-04 16:16:32 -0500

    dfs implementation for bridge logic

--- include/CXXGraph/Graph/Algorithm/Bridges_impl.hpp
+++ include/CXXGraph/Graph/Algorithm/Bridges_impl.hpp
@@ -12,10 +12,13 @@
 /***     Author: ZigRazor ***/
 /***	 E-Mail: zigrazor@gmail.com 				     ***/
 /***********************************************************/
-/***	 Collaboration: ----------- 				     ***/
+/***	 Collaboration: John Ronchetto 				     ***/
 /***********************************************************/
 /***	 License: MPL v2.0 ***/
 /***********************************************************/
+
+// NOT OPTIMIZED SOLUTION
+// UNDIRECTED GRAPH ASSUMPTION
 
 #ifndef __CXXGRAPH_BRIDGES_IMPL_H__
 #define __CXXGRAPH_BRIDGES_IMPL_H__
@@ -32,12 +35,72 @@
 
 namespace CXXGraph {
 
-// returns list of edges that are bridges in the graph
+// Returns list of edges that are bridges in the graph
 template <typename T>
 const std::vector<std::pair<Node<T>, Node<T>>> Graph<T>::bridges(
     const Node<T> &start) const {
 
+	std::vector<std::pair<Node<T>, Node<T>>> bridges;
 
+	auto &nodeSet = start.getNodeSet();
+
+	// Check that start node actually exists
+	auto startNodeIt = std::find_if(
+		nodeSet.begin(), nodeSet.end(),
+		[&start](auto &node) {return node->getUserId() == start.getUserId();});
+	// If start node not in graph, then return wmpty result
+	if (startNodeIt == nodeSet.end())
+		return bridges;
+
+	// Helper that runs dfs on node and skips a specific edge (skipU-SkipV)
+	std::fucntion<void(const std::shared_ptr<AdjacencyList<T>>,
+						shared<const Node<T>>,
+						shared<const Node<T>>,
+						shared<const Node<T>>,
+						std::vector<Node<T>> &)>
+		dfs;
+
+	dfs = [&explore](const std::shared_ptr<AdjacencyList<T>> adj,
+					shared<const Node<T>> node,
+					shared<const Node<T>> skipU,
+					shared<const Node<T>> skipV,
+					std::vector<Node<T>> &visited) -> void {
+
+		// Mark current node as visited
+		visited.push_back(*node);
+
+		// if node has no out edges, then we're done
+		if (adj->find(node) == adj->end()) {
+			return;
+		}
+
+		// Explore all nighbors of current node
+		for (const auto &x : adj->at(node)) {
+			auto next = x.first;
+
+			// Check whether the current edge (node-next) is
+			// the one we want to remove. Check both directions.
+
+			bool edgeRemoved =
+				(node->getUserId() == skipU->getUserId() &&
+				next->getUserId() == skipV->getUserId() ||
+ 				(node->getUserId() == skipV->getUserId() &&
+ 				next->getUserId() == skipU->getUserId());
+
+			// Pretend that edge doesn't exist
+			if (edgeRemoved) {
+				continue;
+			}
+
+			// Standard dfs check using Node<T> value
+			if (std::find(visited.begin(), visited.end(), *next) ==
+				visited.end()) {
+				dfs(adj, next, skipU, skipV, visited);
+			}
+		}
+	};
+
+	//
 
 }
 
